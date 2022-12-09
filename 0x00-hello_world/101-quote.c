ccafa23fd51b46b4984d2743e6d37a57803944e4
SamuelChiemeka <samuelifemegbulem@my.uopeople.edu>
2022-12-08 18:58:38 -0800

    a c program script

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,9 @@
+#!/bin/bash
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+  write(STDOUT_FILENO, "and that piece of art is useful
+\" - Dora Korpar, 2015-10-19\n", 59);
+  return (1);
+}
