bc7673b562b32049ddc567428cfde52ba72559da
SamuelChiemeka <samuelifemegbulem@my.uopeople.edu>
2022-12-12 21:28:01 -0800

    correction on 4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #!/bin/bash
 #include <stdio.h>
+
+/**
+ * main - Writing a program that prints out a string to stdout.
+ * Return: 0 if success.
+ */
+ 
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
+
