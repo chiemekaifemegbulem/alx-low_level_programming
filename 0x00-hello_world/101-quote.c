3229770d727c62df9447b74d11eec234a4db4efb
SamuelChiemeka <samuelifemegbulem@my.uopeople.edu>
2022-12-12 22:00:54 -0800

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,13 @@
-#!/bin/bash
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main - Prints out a predefined statement
+ * Return: 1 if success.
+ */
 int main(void)
 {
-  write(STDOUT_FILENO, "and that piece of art is useful
+write(STDOUT_FILENO, "and that piece of art is useful
 \" - Dora Korpar, 2015-10-19\n", 59);
-  return (1);
+return (1);
 }
+
