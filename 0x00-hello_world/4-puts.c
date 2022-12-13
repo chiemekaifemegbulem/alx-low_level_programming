8478e3391aa247010e690713bc77881dda8f3fe8
SamuelChiemeka <samuelifemegbulem@my.uopeople.edu>
2022-12-12 21:45:24 -0800

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
-#!/bin/bash
-#include <stdio.h>
-
+#include<stdio.h>
 /**
  * main - Writing a program that prints out a string to stdout.
  * Return: 0 if success.
  */
- 
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return (0);
+return (0); 
 }
 
