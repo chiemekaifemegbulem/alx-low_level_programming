f177ae4135f8b83b2a1020b39988c2e9e18c5475
SamuelChiemeka <samuelifemegbulem@my.uopeople.edu>
2022-12-12 21:49:47 -0800

    correction on 5-printf.c file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,11 @@
-#!/bin/bash
-#include <stdio.h>
+#include<stdio.h>
+/**
+ * main - Prints out a string to stdout.
+ * Return: 0 if success.
+ */
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
+
