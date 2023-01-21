df3609929a6d3507eb01e973f9b6fcedcba9d5f4
SamuelChiemeka <samuelifemegbulem@my.uopeople.edu>
2023-01-21 12:10:07 -0800

    a function that searches for an integer.

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+/**
+ * int_index
+ * @array:array
+ * @size: size
+ * @cmp: cmp
+ *
+ * Return: end
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i = 0;
+if (size > 0)
+{
+if (array != NULL && cmp != NULL)
+{
+while (i < size)
+{
+if (cmp(array[i]))
+return (i);
+i++;
+}
+}
+}
+return (-1);
+}
+
